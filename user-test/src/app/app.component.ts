99a9af3d0df6e8e14fd5bf62b87bdbe963579b72
Aidan Killeen <aidan.killeen@gmail.com>
2022-01-11 15:01:40 +0000

    added editing to user component

--- user-test/src/app/app.component.ts
+++ user-test/src/app/app.component.ts
@@ -4,14 +4,73 @@ import { User } from './user.model';
 @Component({
   selector: 'app-root',
   template: `<h1>{{ title }}</h1>
+
+{{ users | json }}
+  <user 
+    *ngFor="let user of users" 
+    [user]="user"
+    (saved)=onSave($event)
+    >
+  </user>
   
-    <user *ngFor="let user of users" [user]="user">
-      
-    </user>
+  {{ name }}
+  <hr>
+  <input #txtName type="text" [value]="name" (keyup)="onChange(txtName.value)"><br>
+  <button (click)="name = 'Aidan'">Reset Name</button>
+
+  <hr>
+  {{ name }}
+
+  <hr>
+
+  <input #txtName2 type="text" [value]="name" (keyup)="onChange(txtName2.value)"><br>
+
+
+
+  <hr>
+  <input [(ngModel)]="county"><br>
+  {{ county }}
+  <hr>
+  <button (click)="county='Limerick'">
+    Reset County
+  </button>
+
+  <hr>
+
+  <!-- two way data-binding -->
+  <input [(ngModel)]="a" type="number"><br>
+  <input [(ngModel)]="b" type="number"><br>
+  <hr>
+  {{ a + b }}
+
+
+
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  a: number = 0;
+  b: number = 0;
+
+  county: string = "Cork";
+
+  name: string = "Aidan";
+
+  onSave(updatedUser:User) {
+    // update the users list with updatedUser
+    let index = this.users.findIndex(user => user.id == updatedUser.id);
+    this.users.splice(index, 1, updatedUser);
+
+  }
+  onChange(changedName: string) {
+    console.log("onChange()");
+    console.log(changedName);
+    this.name = changedName;
+
+  }
+
   title = 'User Test';
 
   users: User[] = [
--- user-test/src/app/user/user.component.ts
+++ user-test/src/app/user/user.component.ts
@@ -1,27 +1,76 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../user.model';
 
 @Component({
   selector: 'user',
   template: `
-    <div [ngClass]="{
+    <div *ngIf="!editing; else editingView" [ngClass]="{
             'active':user.active, 
             'inactive':!user.active
           }">
       {{ user.id }}
+
+
       <h3>{{ user.name }}</h3>
       <a href="mailto:{{ user.email }}">
         {{ user.email }}
       </a>
-      {{ user.active ? "Active" : "Inactive" }}
+
       <hr>
     </div>
+
+    <ng-template #editingView>
+      <div>
+          Name:<input type="text" [(ngModel)]="editingUser.name"><br>
+          Email:<input type="text" [(ngModel)]="editingUser.email"><br>
+          Active:<input type="checkbox" [(ngModel)]="editingUser.active"><br>
+      </div>
+    </ng-template>
+
+    <button *ngIf="!editing" (click)="onEdit()">
+      Edit
+    </button>
+    <button *ngIf="editing" (click)="onSave()">
+      Save
+    </button>
+    <button *ngIf="editing" (click)="onCancel()">
+      Cancel
+    </button><br>
+<!--
+    {{ user | json }}<br>
+    {{ editingUser | json }}<br>
+    <hr>
+        -->
+
   `,
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
+  editing: boolean = false;
   @Input() user:User = new User(0, "", "", false);
+
+  @Output() saved = new EventEmitter();
+
+  editingUser:User = new User(0, "", "", false);
+  
+  onCancel() {
+    this.editing = false;
+  }
+  onSave() {
+    this.editing = false;
+    this.user = this.editingUser;
+    this.saved.emit(this.user);
+  }
+  onEdit() {
+    this.editing = true;
+    this.editingUser = new User(this.user.id, 
+                                this.user.name, 
+                                this.user.email, 
+                                this.user.active);
+  }
+  
+  
   constructor() { }
 
   ngOnInit(): void {
